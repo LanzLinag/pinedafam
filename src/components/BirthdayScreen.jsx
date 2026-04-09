911eb0526246ba1ab3bb304130c5110702d199da
Lanz Elijah P. Linag <140188734+LanzLinag@users.noreply.github.com>
2026-04-09 17:42:45 +0800

    Update BirthdayScreen.jsx

--- src/components/BirthdayScreen.jsx
+++ src/components/BirthdayScreen.jsx
@@ -8,7 +8,7 @@ const BirthdayScreen = () => {
     "July", "August", "September", "October", "November", "December"
   ];
 
-  const daysOfWeek = ["S", "M", "T", "W", "T", "F", "S"];
+  const daysOfWeek = ["S", "M", "T", "W", "TH", "F", "S"];
 
   const getNearestBirthday = () => {
     const today = new Date();
