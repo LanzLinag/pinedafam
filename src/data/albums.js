56dbc179f3eaec312b5e307dd98e1788fc1cbf4e
Lanz Elijah P. Linag <lanzlinag@gmail.com>
2026-04-16 12:03:01 +0800

    updated pics and homepage fixed

--- src/data/albums.js
+++ src/data/albums.js
@@ -8,21 +8,21 @@ const albumsData = [
     coverImage: "" 
   },
 
-    {
+  {
     id: 2,
-    title: "Apu Tey's 80th Birthday",
-    date: "March 2026",
-    description: "",
-    driveLink: "https://drive.google.com/drive/folders/1lheUEGQ4OK3_EVYsjroeR46VcKGL66Js?usp=drive_link",
-    coverImage: ""
-  },
-
-    {
-    id: 3,
     title: "Gaby's 18th Birthday",
     date: "December 2025",
     description: "",
     driveLink: " https://www.facebook.com/share/v/17RiWdWTqg/",
+    coverImage: ""
+  },
+
+  {
+    id: 3,
+    title: "Apu Tey's 80th Birthday",
+    date: "March 2026",
+    description: "",
+    driveLink: "https://drive.google.com/drive/folders/1lheUEGQ4OK3_EVYsjroeR46VcKGL66Js?usp=drive_link",
     coverImage: ""
   },
 
