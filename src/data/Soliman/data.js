60178796ceecf22d60cc91d1992fbc797f369409
Lanz Elijah P. Linag <lanzlinag@gmail.com>
2026-04-16 12:05:30 +0800

    updated pics and homepage

--- src/data/Soliman/data.js
+++ src/data/Soliman/data.js
@@ -1,7 +1,7 @@
 export default [
-  { id: 1, name: "Almira Pineda Soliman", nickname: "Mhye", familyGroup: "Soliman", birthday: "1974-06-04", location: "Las Vegas", image: new URL("./mhye.jpg", import.meta.url).href, description: "Patriarch" },
+  { id: 1, name: "Almira P. Soliman", nickname: "Mhye", familyGroup: "Soliman", birthday: "1974-06-04", location: "Las Vegas", image: new URL("./mhye.jpg", import.meta.url).href, description: "Patriarch" },
   { id: 2, name: "Heric G. Soliman", nickname: "Pogs",  familyGroup: "Soliman", birthday: "1974-02-02", location: "Las Vegas", image: new URL("./pogs.jpg", import.meta.url).href, description: "Matriarch" },
-  { id: 3, name: "Alfred Hermin P. Soliman", nickname: "Red",  familyGroup: "Soliman", birthday: "2002-09-21", location: "Las Vegas", image: new URL("./alfred.jpg", import.meta.url).href, description: "Father" },
+  { id: 3, name: "Alfred Hermin P. Soliman", nickname: "Redd",  familyGroup: "Soliman", birthday: "2001-09-21", location: "Las Vegas", image: new URL("./alfred.jpg", import.meta.url).href, description: "Father" },
   { id: 4, name: "Vince Almer P. Soliman", nickname: "Abing",  familyGroup: "Soliman", birthday: "2005-02-11", location: "Las Vegas", image: new URL("./vince.jpg", import.meta.url).href, description: "Mother" },
   { id: 5, name: "Ross Angelo P. Soliman", nickname: "Ross",  familyGroup: "Soliman", birthday: "2011-11-25", location: "Las Vegas", image: new URL("./ross.jpg", import.meta.url).href, description: "Son" }
 ];
