4a8948551224f742ff319029290680c516fd127c
Lanz Elijah P. Linag <lanzlinag@gmail.com>
2026-04-20 18:23:00 +0800

    Edited nicknames

--- src/data/Cahili/data.js
+++ src/data/Cahili/data.js
@@ -2,5 +2,5 @@ export default [
   { id: 1, name: "Elaine P. Cahili", nickname: "Len", familyGroup: "Cahili", birthday: "1978-09-13", location: "Essel", image: new URL("./elaine.jpg", import.meta.url).href, description: "Matriarch" },
   { id: 2, name: "Alvyn V. Cahili", nickname: "Vyn", familyGroup: "Cahili", birthday: "1973-12-18", location: "Essel", image: new URL("./alvyn.jpg", import.meta.url).href, description: "Patriarch" },
   { id: 3, name: "Angel Shekinah P. Cahili", nickname: "Gel", familyGroup: "Cahili", birthday: "2003-10-13", location: "Essel", image: new URL("./angel.jpg", import.meta.url).href, description: "Father" },
-  { id: 4, name: "Tyrone Lee Angelo P. Cahili", nickname: "Gelo/Palelot", familyGroup: "Cahili", birthday: "2005-10-15", location: "Essel", image: new URL("./gelo.jpg", import.meta.url).href, description: "Mother" },
+  { id: 4, name: "Tyrone Lee Angelo P. Cahili", nickname: "Palelot", familyGroup: "Cahili", birthday: "2005-10-15", location: "Essel", image: new URL("./gelo.jpg", import.meta.url).href, description: "Mother" },
 ];
--- src/data/Pineda/data.js
+++ src/data/Pineda/data.js
@@ -1,7 +1,7 @@
 export default [
   { id: 1, name: "Allan M. Pineda", nickname: "Bap", familyGroup: "Pineda", birthday: "1973-04-06", location: "San Jose", image: new URL("./allan.jpg", import.meta.url).href, description: "Patriarch" },
-  { id: 2, name: "Princess Nicole L. Pineda", nickname: "Col",  familyGroup: "Pineda", birthday: "1999-12-22", location: "New York", image: new URL("./nicole.jpg", import.meta.url).href, description: "Matriarch" },
-  { id: 3, name: "Hanna Mariela L. Pineda", nickname: "Ella", familyGroup: "Pineda", birthday: "2001-06-18", location: "New York", image: new URL("./ella.jpg", import.meta.url).href, description: "Father" },
+  { id: 2, name: "Princess Nicole L. Pineda", nickname: "Cess",  familyGroup: "Pineda", birthday: "1999-12-22", location: "New York", image: new URL("./nicole.jpg", import.meta.url).href, description: "Matriarch" },
+  { id: 3, name: "Hanna Mariela L. Pineda", nickname: "Ellabai", familyGroup: "Pineda", birthday: "2001-06-18", location: "New York", image: new URL("./ella.jpg", import.meta.url).href, description: "Father" },
   { id: 4, name: "Jenny L. Pineda", nickname: "Jen", familyGroup: "Pineda", birthday: "1974-05-26", location: "New York", image: new URL("./jenny.jpg", import.meta.url).href, description: "Father" }
 
 ];
