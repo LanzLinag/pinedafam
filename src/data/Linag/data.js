03e5b37725a4e6198d882e78e39b36a35865614a
Lanz Elijah P. Linag <140188734+LanzLinag@users.noreply.github.com>
2026-05-11 14:02:35 +0800

    Update nickname

--- src/data/Linag/data.js
+++ src/data/Linag/data.js
@@ -2,6 +2,6 @@ export default [
   { id: 1, name: "Esmeralda Pineda Linag", nickname: "May",  familyGroup: "Linag", birthday: "1971-09-11", location: "Dau", image: new URL("./esmeralda.jpg", import.meta.url).href, description: "Father" },
   { id: 2, name: "Lauro N. Linag", nickname: "Dak",  familyGroup: "Linag", birthday: "1972-06-22", location: "Dau", image: new URL("./lauro.jpg", import.meta.url).href, description: "Mother" },
   { id: 3, name: "Eliza Loiusse P. Linag", nickname: "Lai",  familyGroup: "Linag", birthday: "2001-03-18", location: "Dau", image: new URL("./eliza.jpg", import.meta.url).href, description: "Son" },
-  { id: 4, name: "Lanz Elijah P. Linag", nickname: "Patcholos", familyGroup: "Linag", birthday: "2004-06-21 ", location: "Dau", image: new URL("./lanz.jpg", import.meta.url).href, description: "Daughter" },
+  { id: 4, name: "Lanz Elijah P. Linag", nickname: "Poging Bagsik", familyGroup: "Linag", birthday: "2004-06-21 ", location: "Dau", image: new URL("./lanz.jpg", import.meta.url).href, description: "Daughter" },
   { id: 5, name: "Eunize Leigh P. Linag", nickname: "Leigh", familyGroup: "Linag", birthday: "2007-03-28", location: "Dau", image: new URL("./eunize.jpg", import.meta.url).href, description: "Daughter" }
 ];
